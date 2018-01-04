5282e940861d6ca842c0a7126ccaba2f1ef0eace
Morioka Shuhei <selmertsx@gmail.com>
2018-01-04 11:40:26 +0900

    describe test item

--- __tests__/cli.spec.ts
+++ __tests__/cli.spec.ts
@@ -3,7 +3,24 @@ import spawn from 'child_process';
 
 process.chdir(__dirname);
 
+describe('flag や optionの確認',()=>{
+  it('--out-dir で指定されたpathに圧縮後の画像を出力する', () => {
+  });
+
+  it('--out-dir が指定されておらず、--overwrite flagが設定された場合、画像を上書きする', () => {
+  });
+
+  it('--out-dir が指定されておらず、--overwrite flagも設定されていない場合、エラーとなる', ()=> {
+  });
+});
+
+
 describe('compress png images', () => {
-  it('Image need filepath and filesize', async () => {
+  it('指定されたpathに圧縮後の画像を出力する', () => {
+  });
+});
+
+describe('jpgの圧縮', ()=> {
+  it('指定されたpathに圧縮後の画像を出力する', () => {
   });
 });
