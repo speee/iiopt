02b7714f169c5ed2b9e53b328f7ba088eaceafe2
Morioka Shuhei <selmertsx@gmail.com>
2018-01-04 09:53:01 +0900

    yarn add -D execa
    to test compressing images

---
+++ __tests__/cli.spec.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import spawn from 'child_process';
+
+process.chdir(__dirname);
+
+describe('compress png images', () => {
+  it('Image need filepath and filesize', async () => {
+  });
+});
