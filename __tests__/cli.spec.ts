0d2b4ecc0f40ff411029ed2dea32dc9a8dbb0bc5
Morioka Shuhei <selmertsx@gmail.com>
2018-01-04 16:03:32 +0900

    test overwrite images

--- __tests__/cli.spec.ts
+++ __tests__/cli.spec.ts
@@ -28,7 +28,7 @@ describe('flag や optionの確認',()=>{
 
   test('--out-dir が指定されておらず、--overwrite flagも設定されていない場合、エラーとなる', () => {
     try {
-      execa.shellSync('iiopt images/illust.png')
+      execa.shellSync('iiopt images/illust.png');
     } catch(error) {
       expect(error.message).toMatch('--out-dir or --overwrite parameter is needed, specify a `--overwrite`');
     }
@@ -36,7 +36,21 @@ describe('flag や optionの確認',()=>{
 });
 
 describe('上書きの確認', () => {
-  test('--out-dir が指定されておらず、--overwrite flagが設定された場合、画像を上書きする', () => {
+  beforeAll(() => {
+    child_process.execSync('mkdir tmp && cp images/illust.png ./tmp/');
+    console.log('create tmp directory to check overwrite images');
+  });
+
+  afterAll(() => {
+    child_process.execSync('rm -rf tmp');
+    console.log('remove tmp directory');
+  });
+
+  test('--out-dir が指定されておらず、--overwrite flagが設定された場合、画像を上書きする',() => {
+    const rawImage = fs.readFileSync('tmp/illust.png');
+    child_process.execSync('iiopt tmp/illust.png --overwrite');
+    const compressedImage = fs.readFileSync('tmp/illust.png');
+    expect(rawImage.byteLength).toBeGreaterThan(compressedImage.byteLength);
   });
 });
 
