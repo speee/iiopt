5179d23947c230c10e0e486117edcb746641f409
Morioka Shuhei <selmertsx@gmail.com>
2018-01-15 14:27:32 +0900

    test iiopt --apply-new-files

--- __tests__/git_hooks.spec.ts
+++ __tests__/git_hooks.spec.ts
@@ -20,3 +20,26 @@ describe('iiopt --install-git-hooks', () => {
     expect(afterExist).toBeTruthy();
   });
 });
+
+describe('iiopt --apply-new-files', () => {
+  beforeAll(() => {
+    child_process.execSync('rm -f .git/hooks/pre-commit');
+    child_process.execSync('iiopt --install-git-hooks');
+    child_process.execSync('mkdir test_images');
+  });
+
+  afterAll(() => {
+    child_process.execSync('rm -f .git/hooks/pre-commit');
+    child_process.execSync('rm -rf test_images');
+    child_process.execSync('git reset HEAD test_images/illust.png');
+  });
+
+  test('commited raw images are compressed', () => {
+    child_process.execSync('cp images/illust.png ./test_images/ && git add test_images/illust.png');
+    const rawImage = fs.readFileSync('test_images/illust.png');
+    child_process.execSync('./bin/apply_new_files');
+    const compressedImage = fs.readFileSync('test_images/illust.png');
+
+    expect(rawImage.byteLength).toBeGreaterThan(compressedImage.byteLength);
+  });
+});
