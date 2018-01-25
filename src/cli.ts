66f57ddea941a484772658e3c545aa5085814df6
Morioka Shuhei <selmertsx@gmail.com>
2018-01-25 10:30:12 +0900

    remove fs sync functions in cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -35,16 +35,29 @@ const cli = meow(`
   }
 });
 
-if (cli.flags.installGitHooks) {
-  console.log('install script that hooks git pre-commit to compress image automatically');
 
+async function insertPrecommitHooks() {
   const script = `#!/bin/sh -e
   npx iiopt --apply-new-files
   `;
 
-  fs.writeFileSync('.git/hooks/pre-commit', script);
-  fs.chmodSync('.git/hooks/pre-commit', '755');
-  process.exit(0);
+  await fs.writeFile('.git/hooks/pre-commit', script, (err) => {
+    if (err) { throw err; }
+  });
+  await fs.chmod('.git/hooks/pre-commit', '755', (err) => {
+    if (err) { throw err; }
+  });
+}
+
+if (cli.flags.installGitHooks) {
+  console.log('install script that hooks git pre-commit to compress image automatically');
+
+  insertPrecommitHooks().then(
+    process.exit(0)
+  ).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 }
 
 export function run() {
