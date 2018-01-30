7ac86f3bfee7050d636b81e05a493a7fbb584011
Morioka Shuhei <selmertsx@gmail.com>
2018-01-30 13:40:44 +0900

    [refactoring&bugfix] cli.ts
    I use async&await function wrong way, and I fixed it.

--- src/cli.ts
+++ src/cli.ts
@@ -35,32 +35,20 @@ const cli = meow(`
   }
 });
 
-
-async function insertPrecommitHooks() {
+function installGitHooks() {
+  console.log('install script that hooks git pre-commit to compress image automatically');
   const script = `#!/bin/sh -e
   npx iiopt --apply-new-files
   `;
-
-  await fs.writeFile('.git/hooks/pre-commit', script, (err) => {
+  fs.writeFile('.git/hooks/pre-commit', script, { mode: 0o755 } , (err) => {
     if (err) { throw err; }
-  });
-  await fs.chmod('.git/hooks/pre-commit', '755', (err) => {
-    if (err) { throw err; }
-  });
-}
-
-if (cli.flags.installGitHooks) {
-  console.log('install script that hooks git pre-commit to compress image automatically');
-  insertPrecommitHooks().then(
-    process.exit(0)
-  ).catch((err) => {
-    console.error(err);
-    process.exit(1);
   });
 }
 
 export function run() {
-  if ( cli.flags.applyNewFiles ) {
+  if ( cli.flags.installGitHooks ) {
+    installGitHooks();
+  } else if ( cli.flags.applyNewFiles ) {
     ApplyNewFiles.run(cli.flags)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
