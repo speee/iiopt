b4ab55898b59837de134e76f7c0e51555c17f5b6
Morioka Shuhei <selmertsx@gmail.com>
2018-01-31 16:09:49 +0900

    asynchronize __tests__/overwrite.spec.ts

--- __tests__/overwrite.spec.ts
+++ __tests__/overwrite.spec.ts
@@ -1,33 +1,32 @@
 import fs from 'fs';
 import execa from 'execa';
 import child_process from 'child_process';
+import { promisify } from 'util';
+
+const processExecAsync = promisify(child_process.exec);
+const readFileAsync = promisify(fs.readFile);
 
 describe('overwrite images', () => {
-  beforeAll(() => {
-    child_process.execSync('rm -rf tmp && mkdir tmp && cp images/* tmp');
-  });
+  afterAll( async () => { await processExecAsync('rm -rf tmp'); });
+  beforeAll( async () => { await processExecAsync('rm -rf tmp && mkdir tmp && cp images/* tmp'); });
 
-  afterAll(() => {
-    child_process.execSync('rm -rf tmp');
-  });
-
-  test("if --overwrite flag is set and --out-dir isn't set, a jpg image is overwritten", () => {
-    const rawImage = fs.readFileSync('tmp/sample.jpg');
-    child_process.execSync('bin/cli ./tmp/sample.jpg --overwrite');
-    const compressedImage = fs.readFileSync('tmp/sample.jpg');
+  test("if --overwrite flag is set and --out-dir isn't set, a jpg image is overwritten", async () => {
+    const rawImage = await readFileAsync('tmp/sample.jpg');
+    await processExecAsync('bin/cli ./tmp/sample.jpg --overwrite');
+    const compressedImage = await readFileAsync('tmp/sample.jpg');
     expect(rawImage.byteLength).toBeGreaterThan(compressedImage.byteLength);
   });
 
-  test("raw png image is given", () => {
-    const rawImage = fs.readFileSync('tmp/illust.png');
-    child_process.execSync('bin/cli ./tmp/illust.png --overwrite');
-    const compressedImage = fs.readFileSync('tmp/illust.png');
+  test("raw png image is given", async () => {
+    const rawImage = await readFileAsync('tmp/illust.png');
+    await processExecAsync('bin/cli ./tmp/illust.png --overwrite');
+    const compressedImage = await readFileAsync('tmp/illust.png');
     expect(rawImage.byteLength).toBeGreaterThan(compressedImage.byteLength);
   });
 
-  test("optimized png image is given", () => {
-    const beforeImage = fs.readFileSync('tmp/illust_optimized.png');
-    const result =  child_process.execSync('bin/cli ./tmp/illust_optimized.png --overwrite').toString();
-    expect(result).toContain('./tmp/illust_optimized.png is already optimized');
+  test("optimized png image is given", async () => {
+    const beforeImage = await readFileAsync('tmp/illust_optimized.png');
+    const result =  await processExecAsync('bin/cli ./tmp/illust_optimized.png --overwrite');
+    expect(result.stdout).toContain('./tmp/illust_optimized.png is already optimized');
   });
 });
