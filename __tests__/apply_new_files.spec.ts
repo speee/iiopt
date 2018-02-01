7e89c954d7da1694250a796698a78a7c0b752cbc
Morioka Shuhei <selmertsx@gmail.com>
2018-02-01 12:43:39 +0900

    refactoring apply_new_files.spec.ts

--- __tests__/apply_new_files.spec.ts
+++ __tests__/apply_new_files.spec.ts
@@ -1,28 +1,33 @@
 import fs from 'fs';
 import child_process from 'child_process';
 import { promisify } from 'util';
-
 import * as ApplyNewFiles from '../src/apply_new_files';
+
+const unlinkAsync = promisify(fs.unlink);
+const mkdirAsync = promisify(fs.mkdir);
+const readFileAsync = promisify(fs.readFile);
+const copyFileAsync = promisify(fs.copyFile);
+const execAsync = promisify(child_process.exec);
 
 describe('iiopt --apply-new-files', () => {
   beforeAll( async () => {
-    await promisify(fs.unlink)('.git/hooks/pre-commit').catch(err => {});
-    await promisify(child_process.exec)('bin/cli --install-git-hooks');
+    await unlinkAsync('.git/hooks/pre-commit').catch(err => {});
+    await execAsync('bin/cli --install-git-hooks');
     await promisify(fs.mkdir)('./test_images');
   });
 
   afterAll( async () => {
-    await promisify(fs.unlink)('.git/hooks/pre-commit');
-    await promisify(child_process.exec)('git reset HEAD test_images/sample.jpg');
-    await promisify(child_process.exec)('rm -rf test_images');
+    await unlinkAsync('.git/hooks/pre-commit');
+    await execAsync('git reset HEAD test_images/sample.jpg');
+    await execAsync('rm -rf test_images');
   });
 
   test('commited raw images are compressed', async () => {
-    await promisify(fs.copyFile)('images/sample.jpg', './test_images/sample.jpg');
-    await promisify(child_process.exec)('git add test_images/sample.jpg');
-    const rawImage = await promisify(fs.readFile)('test_images/sample.jpg');
-    await promisify(child_process.exec)('bin/cli --apply-new-files');
-    const compressedImage = await promisify(fs.readFile)('test_images/sample.jpg');
+    await copyFileAsync('images/sample.jpg', './test_images/sample.jpg');
+    await execAsync('git add test_images/sample.jpg');
+    const rawImage = await readFileAsync('test_images/sample.jpg');
+    await execAsync('bin/cli --apply-new-files');
+    const compressedImage = await readFileAsync('test_images/sample.jpg');
     expect(rawImage.byteLength).toBeGreaterThan(compressedImage.byteLength);
   });
 });
