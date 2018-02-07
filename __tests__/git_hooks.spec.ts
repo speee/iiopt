e45bd2b12a4ddb8db1a7348306bb225ae40598ee
Morioka Shuhei <selmertsx@gmail.com>
2018-02-07 14:23:05 +0900

    [bugfix] async error handling in git_hooks.spec.ts

--- __tests__/git_hooks.spec.ts
+++ __tests__/git_hooks.spec.ts
@@ -7,8 +7,13 @@ const exitAsync = promisify(fs.exists);
 const unlinkAsync = promisify(fs.unlink);
 
 describe("iiopt --install-git-hooks", () => {
-  beforeAll(async () => await unlinkAsync(".git/hooks/pre-commit"));
-  afterAll(async () => await unlinkAsync(".git/hooks/pre-commit"));
+  beforeAll(
+    async () => await unlinkAsync(".git/hooks/pre-commit").catch(err => {})
+  );
+
+  afterAll(
+    async () => await unlinkAsync(".git/hooks/pre-commit").catch(err => {})
+  );
 
   test("add .git/hooks/pre-commit", async () => {
     const beforeExist = await exitAsync(".git/hooks/pre-commit");
